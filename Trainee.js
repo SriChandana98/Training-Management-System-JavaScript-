0be47ea6b8b83359af92e3d51423ae6712fece9f
Kaushani RC <kaushanirc97@gmail.com>
2019-08-11 20:29:18 -0700

    Add files via upload

---
+++ Trainee.js
@@ -0,0 +1,33 @@
+function trainee()
+{
+    this.Trainee = [];
+    this.insert = function(traineeObj)
+    {
+        this.trainee.push(traineeObj)
+    }
+
+this.display = function()
+{
+    this.console.log(this.Trainee)
+}
+this.delete = function(index)
+{
+    this.Trainee.splice(index, 1);
+}
+}
+var obj =new Trainee();
+var trainee1 = {name: "Kaushani", email: "cautioni@gmail.com", phoneNo : 8961686073};
+
+var trainee2 = {name: "Chandana", email: "chandana@gmail.com", phoneNo : 8961234676};
+
+var trainee3 = {name: "Riya", email: "RiyaB@gmail.com", phoneNo : 8961684563};
+
+obj.insert(trainee1);
+obj.insert(trainee2);
+obj.insert(trainee3);
+console.log("display after insertion");
+obj.display();
+
+console.log("display after deletion");
+obj.delete(1);
+obj.display();
